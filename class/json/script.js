580c8012262fa48bb3f20592d5766df1d5d05ed5
joshuamitchell <joshuamitchell176@gmail.com>
2019-10-28 15:24:52 -0400

    json

--- class/json/script.js
+++ class/json/script.js
@@ -1,17 +1,32 @@
 "using strict"
 
-async function special() {
-    console.log("In Special Beginning");
-    let result = await setTimeout(doStuff, 2000);
-    console.log("In Special Ending");
-}
+async function displayShoe() {
+    let response = await fetch("https://joshuamitchell.github.io/class/json/shoe.json");
+    let shoeJson = await response.json();
 
-function doStuff() {
-    console.log("In do Stuff");
+    let shoeName = shoeJson.name;
+    let shoeBrand = shoeJson.brand;
+    let comments = shoeJson.comments;
+
+    console.log(`${shoeName} by ${shoeBrand}`)
+
+    for (i in comments) {
+        console.log(comments[i]);
+    }
+
+    // append a section with info about the shoe to your page
+    let section = document.createElement("section");
+    section.innerHTML = `${shoeName} by ${shoeBrand}`;
+    let content = document.getElementById("content");
+    content.append(section);
 }
 
 window.onload = function() {
-    this.console.log("Before calling Special");
-    this.special();
-    this.console.log("After calling Special");
+    this.displayShoe();
+
+    // append a h1 to your page
+    let content = document.getElementById("content");
+    let h1 = document.createElement("h1");
+    h1.innerHTML = "I love shoes";
+    content.append(h1);
 }
